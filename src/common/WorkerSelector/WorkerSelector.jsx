0413e7ce6e5788549163d0752ee53d0be3643b7e
Christian <christianperez.rbb@gmail.com>
2023-11-21 17:51:56 +0100

    feat: create custom component WorkerSelector.

---
+++ src/common/WorkerSelector/WorkerSelector.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import './WorkerSelector.css'
+import { GetWorkers } from '../../services/apiCalls'
+
+export const WorkerSelector = ({design, type, name, placeholder,value, disabled, functionProp}) => {
+
+    const [workers, setWorkers] = useState([])
+    useEffect (()=> {
+
+        GetWorkers()
+        .then(
+            result => {
+                if(result.data.workers.length > 0 ){
+                    setWorkers(result.data.workers)
+                }
+            }
+        )
+        .catch(error => console.log(error))
+
+    }, [])
+
+     return (
+        
+         <select
+            className={design}
+            name={name}
+            value={value || undefined}
+            onChange={(e)=>{functionProp(e);console.log(e)}}  >  
+            {workers.map((worker) =>{
+                return(
+                    <option value={worker.id}>{worker.name}</option>
+                )
+            })}
+
+         </select>
+     )
+}
--- src/pages/CreateAppointment/CreateAppointment.jsx
+++ src/pages/CreateAppointment/CreateAppointment.jsx
@@ -5,97 +5,98 @@ import "./CreateAppointment.css";
 import { userData } from "../userSlice";
 import { useSelector } from "react-redux";
 import { ImputAppointments } from "../../common/ImputAppointments/ImputAppointments";
-
+import { WorkerSelector } from "../../common/WorkerSelector/WorkerSelector";
 
 export const Newappointment = () => {
-    const navigate = useNavigate();
-    const datosRdxUser = useSelector(userData);
-    const token = datosRdxUser.credentials.token;
+  const navigate = useNavigate();
+  const datosRdxUser = useSelector(userData);
+  const token = datosRdxUser.credentials.token;
 
+  const [create, setCreate] = useState({
+    title: "",
+    description: "",
+    date: "",
+    turn: "",
+    worker: "",
+    client: datosRdxUser.credentials.user.name,
+  });
 
-  
-    const [create, setCreate] = useState({
-      title: "",
-      description: "",
-      date: "",
-      turn: "",
-      worker: "",
-      client: datosRdxUser.credentials.user.name,
-    });
-  
-    const functionHandler = (e) => {
-      setCreate((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value,
-      }));
-    };
-  
-    const Submit = () => {
-      for (let test1 in create) {
-        if (create[test1] === "") {
-          return;
-        }
+  const functionHandler = (e) => {
+    setCreate((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const Submit = () => {
+    for (let test1 in create) {
+      console.log(create[test1], test1);
+      if (create[test1] === "") {
+        return;
       }
-  
-  
-      CreateAppointment(token)
-        .then((resultado) => {
-          console.log(resultado);
-  
-        //   setTimeout(() => {
-        //     navigate("/profile");
-        //   }, 500);
-        })
-        .catch((error) => console.log(error));
-    };
-    return (
-        <div className="createDesign">
-          <div className="Name">Titulo</div>
-          <ImputAppointments
-            disabled={false}
-            design={"inputDesign"}
-            type={"text"}
-            name={"title"}
-            placeholder={""}
-            value={""}
-            functionProp={functionHandler}
-            
-          />
-          <div className="Name">Descripción</div>
-          <ImputAppointments
-            disabled={false}
-            design={"inputDesign"}
-            type={"text"}
-            name={"description"}
-            placeholder={""}
-            value={""}
-            functionProp={functionHandler}
-            
-          />
-          <div className="Name">Fecha</div>
-          <ImputAppointments
-            disabled={false}
-            design={"inputDesign"}
-            type={"date"}
-            name={"date"}
-            placeholder={""}
-            value={""}
-            functionProp={functionHandler}
-            
-          />
-          <div className="Name">Turno</div>
-          <ImputAppointments
-            disabled={false}
-            design={"inputDesign"}
-            type={"text"}
-            name={"turn"}
-            placeholder={""}
-            value={""}
-            functionProp={functionHandler}
-            
-          />
-          <div className="Name">Trabajador</div>
-          <ImputAppointments
+    }
+
+    CreateAppointment(token)
+      .then((resultado) => {
+        console.log(resultado);
+
+        navigate("/profile");
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div className="createDesign">
+      <div className="Name">Titulo</div>
+      <ImputAppointments
+        disabled={false}
+        design={"inputDesign"}
+        type={"text"}
+        name={"title"}
+        placeholder={""}
+        value={""}
+        functionProp={functionHandler}
+      />
+      <div className="Name">Descripción</div>
+      <ImputAppointments
+        disabled={false}
+        design={"inputDesign"}
+        type={"text"}
+        name={"description"}
+        placeholder={""}
+        value={""}
+        functionProp={functionHandler}
+      />
+      <div className="Name">Fecha</div>
+      <ImputAppointments
+        disabled={false}
+        design={"inputDesign"}
+        type={"date"}
+        name={"date"}
+        placeholder={""}
+        value={""}
+        functionProp={functionHandler}
+      />
+      <div className="Name">Turno</div>
+      <ImputAppointments
+        disabled={false}
+        design={"inputDesign"}
+        type={"text"}
+        name={"turn"}
+        placeholder={""}
+        value={""}
+        functionProp={functionHandler}
+      />
+      <div className="Name">Trabajador</div>
+      <WorkerSelector
+        disabled={false}
+        design={"inputDesign"}
+        type={"text"}
+        name={"worker"}
+        placeholder={""}
+        value={""}
+        functionProp={functionHandler}
+      />
+      {/* <ImputAppointments
             disabled={false}
             design={"inputDesign"}
             type={"text"}
@@ -104,21 +105,21 @@ export const Newappointment = () => {
             value={""}
             functionProp={functionHandler}
             
-          />
-          <div className="Name">Cliente</div>
-          <ImputAppointments
-            disabled={true}
-            design={"inputDesign"}
-            type={"text"}
-            name={"client"}
-            placeholder={""}
-            value={datosRdxUser.credentials.user.name}
-            functionProp={functionHandler}
-            
-          />
-    
-          <div className="buttonSubmit" onClick={Submit}>Submit</div>
-        </div>
-      );
-    };
-    
+          /> */}
+      <div className="Name">Cliente</div>
+      <ImputAppointments
+        disabled={true}
+        design={"inputDesign"}
+        type={"text"}
+        name={"client"}
+        placeholder={""}
+        value={datosRdxUser.credentials.user.name}
+        functionProp={functionHandler}
+      />
+
+      <div className="buttonSubmit" onClick={Submit}>
+        Submit
+      </div>
+    </div>
+  );
+};
