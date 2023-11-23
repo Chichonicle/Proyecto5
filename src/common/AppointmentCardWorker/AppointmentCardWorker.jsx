0004af04a82b091462fc67a4d946835442505c20
Christian <christianperez.rbb@gmail.com>
2023-11-23 15:47:46 +0100

    feat: Create card for worker appointments custom card.

---
+++ src/common/AppointmentCardWorker/AppointmentCardWorker.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AppointmentCardWorker.css'
+
+export const AppointmentCardWorker = ({title, description, date, turn, client}) => {
+     return (
+        <div className='AppointmentCard'>
+            <div>Titulo: {title}</div>
+            <div>Descripción: {description}</div>
+            <div>Fecha: {date}</div>
+            <div>Turno: {turn}</div>
+            <div>Cliente: {client} </div>
+        </div>
+
+     )
+}
